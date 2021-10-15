c563651ccd0e91c371463e22a56bd1fe5abb2910
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-15 17:51:15 -0400

    added spinning animation

--- script.js
+++ script.js
@@ -8,15 +8,9 @@ document.querySelector("form").addEventListener("submit", (event)=>{
     event.preventDefault();
 
     if (Math.floor(Math.random() * 3) === numArr[0]) {
-        console.log(`${headOrTails[0]} won`);
-        // side.textContent = "Heads";
-        // side.style.color = "grey";
         coin.style.backgroundImage = "url('./assets/heads.png')";
         coin.style.backgroundSize = "contain";
     } else {
-        console.log(`${headOrTails[1]} won`);
-        // side.textContent = "Tails";
-        // side.style.color = "grey";
         coin.style.backgroundImage = "url('./assets/tails.png')";
         coin.style.backgroundSize = "contain";
     }
