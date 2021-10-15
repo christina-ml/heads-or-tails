5e63bfbbf1a30102dbdefe48fc6f487715189003
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-15 15:05:15 -0400

    made side one div. Switching out text content

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 let coin = document.querySelector("#coin");
-let heads = document.querySelector("#heads");
-let tails = document.querySelector("#tails");
+let side = document.querySelector("#side");
 
 let headOrTails = ["heads", "tails"];
 let numArr = [1, 2];
-
-tails.classList.add("hidden");
-heads.classList.add("hidden");
-
 
 document.querySelector("form").addEventListener("submit", (event)=>{
     event.preventDefault();
 
     if (Math.floor(Math.random() * 3) === numArr[0]) {
         console.log(`${headOrTails[0]} won`);
-        heads.style.color = "red";
-        heads.classList.remove("hidden");
-        tails.classList.add("hidden");
+        side.textContent = "Heads";
+        side.style.color = "red";
     } else {
         console.log(`${headOrTails[1]} won`);
-        tails.style.color = "red";
-        tails.classList.remove("hidden");
-        heads.classList.add("hidden");
+        side.textContent = "Tails";
+        side.style.color = "red";
     }     
 })
